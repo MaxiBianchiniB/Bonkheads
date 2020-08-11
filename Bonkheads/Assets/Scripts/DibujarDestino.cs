78e21984f2c69690dc02a70f083236e7a555119e
MaxiBianchiniB <bianchinimaxibosko@gmail.com>
2020-08-11 18:52:34 -0300

    Add Plataforma Movil

---
+++ Bonkheads/Assets/Scripts/DibujarDestino.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DibujarDestino : MonoBehaviour
+{
+    public Transform inicio, destino;
+    private void OnDrawGizmosSelected()
+    {
+        if (inicio != null && destino != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(inicio.position, destino.position);
+        }
+    }
+}
---
+++ Bonkheads/Assets/Scripts/PlataformaMovil.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovil : MonoBehaviour
+{
+    public Transform Destino;
+    public float speed;
+
+    private Vector3 start, end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Destino != null)
+        {
+            Destino.parent = null;
+            start = transform.position;
+            end = Destino.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Destino != null)
+        {
+            float fixedspeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Destino.position, fixedspeed);
+        }
+
+        if (transform.position == Destino.position)
+        {
+            Destino.position = (Destino.position == start) ? end : start;
+        }
+    }
+}
