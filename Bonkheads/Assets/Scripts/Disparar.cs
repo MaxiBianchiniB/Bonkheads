ce434f2bc641f13aba703906a8c6ed9dbaadea23
Maxi Bianchini Bosko <bianchiniaxibosko@gmail.com>
2020-08-23 14:34:03 -0300

    Add Disparar

---
+++ Bonkheads/Assets/Scripts/Disparar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{
+    public Transform puntoinstancia;
+    public GameObject Bala;
+
+    private float tiempodisparo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempodisparo += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S) && tiempodisparo >= 1f)
+        {
+            Instantiate(Bala, puntoinstancia.position, Quaternion.identity);
+            tiempodisparo = 0f;
+        }
+    }
+}
