b4101e1c0a2f15b34e64486062ec2ffbe7c84805
Maxi Bianchini Bosko <bianchiniaxibosko@gmail.com>
2020-08-23 14:33:39 -0300

    Add Bala

---
+++ Bonkheads/Assets/Scripts/BalaControlador.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaControlador : MonoBehaviour
+{
+    public float MaxSpeed;
+    public float speed;
+
+    private Rigidbody2D Bala;
+
+    //public GameObject Player;
+    private GameObject player;
+    private Transform playertransform;
+
+    private float TiempoMuerte = 3f;
+
+    private void Awake()
+    {
+        Bala = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playertransform = player.transform;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player.transform.localScale.x > 0)
+        {
+            Bala.velocity = new Vector2(speed, Bala.position.y);
+           // transform.localScale = new Vector3(1f, 1f, 1f);   //para flipear la bala
+        }
+        else
+        {
+            Bala.velocity = new Vector2(-speed, Bala.position.y);
+           // transform.localScale = new Vector3(-1f, 1f, 1f); // para flipear la bala
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, TiempoMuerte);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Plataforma")
+        {
+
+            Destroy(gameObject);
+
+        }
+
+        if (collision.gameObject.tag == "Plataforma Movil")
+        {
+            Destroy(gameObject);
+        }
+
+
+        if (collision.gameObject.tag == "Pared")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+
+    public void EliminarBala()
+    {
+        Destroy(gameObject);
+    }
+}
