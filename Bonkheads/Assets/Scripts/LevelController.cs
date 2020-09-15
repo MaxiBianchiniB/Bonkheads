3a53f36095a49da668fa3894c252bc3adfb798c0
MaxiBianchiniB <bianchinimaxibosko@gmail.com>
2020-09-15 19:24:50 -0300

    Add LevelController

---
+++ Bonkheads/Assets/Scripts/LevelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+ 
+    public void CargarLevel(string NombreLevel)
+    { 
+        SceneManager.LoadScene(NombreLevel);
+    }
+}
